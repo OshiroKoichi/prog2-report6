0845f1602a37ba7a782e09e491f0d601433244f9
tameikip <e235707@ie.u-ryukyu.ac.jp>
2023-12-18 11:41:49 +0900

    report6-4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 package main.java;
 
+import main.java.jp.ac.uryukyu.e235707.RedYopu;
 import main.java.jp.ac.uryukyu.e235707.Yopuyopu;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Yopuyopu yopu = new Yopuyopu("よぷ");
-        yopu.yopu();
+       Random random = new Random();
+       int randomyopu = random.nextInt(3);
+        if (randomyopu == 0){
+            RedYopu red = new RedYopu("赤よぷ");
+        }
+        
     }
+
+    
 }
