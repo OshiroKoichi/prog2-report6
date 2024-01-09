b875425e91d696c9b24502e2119e0b22a9984d90
tameikip <e235707@ie.u-ryukyu.ac.jp>
2024-01-09 20:30:26 +0900

    report6-6

---
+++ src/test/java/jp/ac/uryukyu/e235707/YopuTest.java
@@ -0,0 +1,15 @@
+package test.java.jp.ac.uryukyu.e235707;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class YopuTest {
+    @Test
+    public void testYopu(){
+        Generator gneTest = new Generator();
+        gneTest.generate(0);
+        assertEquals()
+    }
+}
